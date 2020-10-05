d304f18949659993f866b7aea066830cf7fafb4c
Gonzalo <gonzalom97@outlook.com>
2020-10-05 16:15:50 -0300

    feat: States para presupuesto y el restante

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pregunta from './components/Pregunta';
 
 function App() {
+	const [presupuesto, setPresupuesto] = useState(0);
+	const [restante, setRestante] = useState(0);
+
 	return (
 		<div className='container'>
 			<header>
 				<h1 className='display-3 mb-5 text-light'>Gasto Semanal</h1>
 
 				<div className='contenido-principal contenido p-5'>
-					<Pregunta />
+					<Pregunta setPresupuesto={setPresupuesto} setRestante={setRestante} />
 				</div>
 			</header>
 		</div>
