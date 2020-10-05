4f2c6399019a786fbf623c3c2a10e80e2f377d4c
Gonzalo <gonzalom97@outlook.com>
2020-10-05 17:48:32 -0300

    feat: Guardar state gasto ingresado

--- src/components/FormularioGastos.jsx
+++ src/components/FormularioGastos.jsx
@@ -1,6 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const FormularioGastos = () => {
+	const [nombreGasto, setNombreGasto] = useState('');
+	const [cantidadGasto, setCantidadGasto] = useState(0);
+
+	const onChanged = (e) => {
+		if (e.target.name === 'cantidad') {
+			let cantidadIngresada = e.target.value;
+			if (cantidadIngresada <= 0 || isNaN(cantidadIngresada)) {
+        // TODO Error de cantidad
+        return;
+      }
+      
+			setCantidadGasto(parseInt(e.target.value));
+		} else {
+			setNombreGasto(e.target.value);
+		}
+	};
+
 	return (
 		<Fragment>
 			<h3>Agrega tus gastos</h3>
@@ -11,6 +28,8 @@ const FormularioGastos = () => {
 					<input
 						type='text'
 						className='form-control'
+						onChange={onChanged}
+						value={nombreGasto}
 						name='nombre'
 						placeholder='Ej. Transporte'
 					/>
@@ -21,6 +40,8 @@ const FormularioGastos = () => {
 					<input
 						type='number'
 						className='form-control'
+						onChange={onChanged}
+						value={cantidadGasto}
 						name='cantidad'
 						placeholder='Ej. 300'
 					/>
