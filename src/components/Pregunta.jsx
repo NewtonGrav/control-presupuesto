4d46de9dc2abf664555f3602c5e8d082a983d0fd
Gonzalo <gonzalom97@outlook.com>
2020-10-05 15:36:58 -0300

    feat: State para presupuesto y su validacion

--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,14 +1,35 @@
 import React, { Fragment } from 'react';
 
 const Pregunta = () => {
+	const [presupuesto, setPresupuesto] = useState(0);
+
+	const presupuestoDefinido = (e) => {
+		setPresupuesto(parseInt(e.target.value));
+	};
+
+	const agregarPresupuesto = (e) => {
+		e.preventDefault();
+    
+		// Validar
+		if (presupuesto < 1 || isNaN(presupuesto)) {
+      setErrorPresupuesto(true);
+      return
+		}
+
+		setErrorPresupuesto(false);
+
+		// Accion
+	};
+
 	return (
 		<Fragment>
 			<h3 className="display-6 mb-5">Coloca tu presupuesto</h3>
 
-			<form action=''>
+			<form onSubmit={agregarPresupuesto}>
 				<div className='mb-4'>
 					<input
 						type='number'
+						onChange={presupuestoDefinido}
 						className='form-control form-control-lg'
 						placeholder='Presupuesto'
 					/>
