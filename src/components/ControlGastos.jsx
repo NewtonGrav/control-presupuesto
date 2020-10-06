4230facc97b67bf3b16340589851df729835835a
Gonzalo <gonzalom97@outlook.com>
2020-10-06 16:20:59 -0300

    style: Posicion de alertas

--- src/components/ControlGastos.jsx
+++ src/components/ControlGastos.jsx
@@ -5,12 +5,12 @@ import { revisarPresupuesto } from './helpers';
 const ControlGastos = ({ presupuesto, restante }) => {
 	return (
 		<Fragment>
-			<Alerta texto={`Presupuesto: $ ${presupuesto}`} tipo='primary' />
-
 			<Alerta
 				texto={`Restante: $ ${restante}`}
 				tipo={revisarPresupuesto(presupuesto, restante)}
 			/>
+      
+			<Alerta texto={`Presupuesto: $ ${presupuesto}`} tipo='primary' />
 		</Fragment>
 	);
 };
