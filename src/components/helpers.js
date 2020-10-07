06ffff15ec3460563bdfe566c2a178119c3f8584
Gonzalo <gonzalom97@outlook.com>
2020-10-07 10:26:23 -0300

    fix:; Calculo porcentaje restante gastando

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,6 +1,6 @@
 export const revisarPresupuesto = (presupuesto, restante) => {
-	const porcentajeRestanteGastado = ((presupuesto - restante) / restante) * 100;
-
+	const porcentajeRestanteGastado = ((presupuesto - restante) / presupuesto) * 100;
+	debugger
 	if (porcentajeRestanteGastado >= 70) return 'danger';
 	else if (porcentajeRestanteGastado >= 40) return 'warning';
 	else return 'success';
